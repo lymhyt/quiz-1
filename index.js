59c786c5ea292878834461d3f645038011fcd395
lymhyt <hayatiyt62@gmail.com>
2023-03-27 15:16:12 +0800

    exercise1

---
+++ index.js
@@ -0,0 +1,25 @@
+//exercise 1
+let data1= true
+let data2 = "1.25"
+//console.log(data1, typeof data1)
+//console.log(data2, typeof data2)
+
+let data3 =data1.toString()+parseFloat(data2)
+//console.log(data3, typeof data3)           
+
+//array
+let data = ["one", 2, true]
+//console.log(data[2], typeof data[2])
+
+//object
+let user ={
+    name :"yat",
+    faculty: "FKEKK",
+    phone:{
+        office:"1234",
+        mobile:"6789"
+},
+    email:"yat@utem.edu.my"
+}
+//console.log( user.faculty)
+console.log(user.phone.mobile )
